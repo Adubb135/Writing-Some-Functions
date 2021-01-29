b4fe3e2f4c630be611ced718201fef9076bb070d
Andrew White <66645240+Adubb135@users.noreply.github.com>
2021-01-29 13:43:09 -0800

    Implemented Square Root Function and more (#7)
    * Implemented square root function
    * Implemented Absolute Value function inside the Square root function as a "Helper" function to be able to help square the number given if it is not a positive float number.
    * Wrote code to prompt user input and calculate the GCD, Absolute Value and Square Root

--- WritingSomeFunctions.c
+++ WritingSomeFunctions.c
@@ -1,13 +1,35 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
+
+int number1 = 0;
+int number2 = 0;
+float number3 = 0.0;
+float number4 = 0.0;
 
 int GCD(int a, int b);
-float ABSV(int c);
-int SQRT(int d);
+float ABSV(float c);
+float SQRT(float d);
 
 int main()
 {
+  // Prompts user to input 2 numbers to find GCD and outputs the number
+  printf("Please enter 2 numbers to find the Greatest Common Denominator: \n");
+  scanf("%d %d", &number1, &number2);
+  int result = GCD(number1, number2);
+  printf("The Greatest Common Denominator between %d and %d is %d!\n\n", number1, number2, result);
 
+  // Prompts user to input a number to find the Absolute Value and outputs the number 
+  printf("Please enter a number to find the Absolute Value: \n");
+  scanf("%fl", &number3);
+  float result2 = ABSV(number3);
+  printf("The Absolute value of %.1fl is %fl.1!\n\n", number3, result2);
+
+  // Prompts user to input a number to find the Square Root and outputs the number
+  printf("Please enter a number to find the Square Root: \n");
+  scanf("%fl", &number4);
+  float result3 = SQRT(number4);
+  printf("The Square Root of %.fl is %.fl!\n\n", number4, result3);
 
   return 0;
 }
@@ -16,14 +38,16 @@ int main()
 // Greatest Common Denominator Implementation
 int GCD(int a, int b)
 {
+  if (a < b)
+    return GCD(b, a);
+
   if (b == 0)
     return a;
+
   return GCD(b, a % b);
 }
 
-
-// Absolute Value Implementation
-float ABSV(int c)
+float ABSV(float c)
 {
   if (c < 0)
     return -(c);
@@ -31,7 +55,12 @@ float ABSV(int c)
 }
 
 // Square Root Implementation
-int SQRT(int d)
+float SQRT(float d)
 {
-  return 0;
+  if ( d < 0.0)
+  {
+    printf("Error 404: In the SQRT function, no negative numbers are allowed. I'll fix it for you");
+    d = ABSV(d);
+  }  
+  return sqrt(d);
 }
